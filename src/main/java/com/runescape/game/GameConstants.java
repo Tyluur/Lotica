2424a11a3310cb1d84938a434094ed9a1f36f71e
Tyluur <itstyluur@icloud.com>
2021-01-28 16:33:15 -0500

    fix cache read dir [2]

--- src/main/java/com/runescape/game/GameConstants.java
+++ src/main/java/com/runescape/game/GameConstants.java
@@ -4,157 +4,158 @@ import com.runescape.game.world.WorldTile;
 
 public final class GameConstants {
 
-	/**
-	 * General client and server settings.
-	 */
-	public static final String SERVER_NAME = "Lotica";
+    /**
+     * General client and server settings.
+     */
+    public static final String SERVER_NAME = "Lotica";
 
-	/**
-	 * If we're hosted on linux
-	 */
-	public static final boolean LINUX_HOST = System.getProperty("os.name").toLowerCase().contains("linux");
+    /**
+     * The path the cache will be loaded from
+     */
+    public static final String CACHE_PATH = "./data/cache/";
 
-	/**
-	 * The times for double experieince
-	 */
-	public static final long[] DOUBLE_EXPERIENCE_TIMES = new long[2];
+    /**
+     * If we're hosted on linux
+     */
+    public static final boolean LINUX_HOST = System.getProperty("os.name").toLowerCase().contains("linux");
 
-	/**
-	 * The build of the client
-	 */
-	public static final int CLIENT_BUILD = 667;
+    /**
+     * The server is on hosted mode if the main user name contains 'Administrator'
+     */
+    public static boolean HOSTED = System.getProperty("user.home").toLowerCase().contains("root") || LINUX_HOST;
 
-	/**
-	 * The custom build of the client, shows what version we're on
-	 */
-	public static final int CUSTOM_CLIENT_BUILD = 3;
+    /**
+     * The path for files to be saved at
+     */
+    public static final String FILES_PATH = HOSTED ? LINUX_HOST ? "/root/gamedata/" : "C:/gamedata/" : "data/";
 
-	/**
-	 * The port the server listens on
-	 */
-	public static final int GAME_PORT_ID = 43594;
+    /**
+     * We will always be on debug mode if the server isn't hosted
+     */
+    public static boolean DEBUG = !HOSTED;
 
-	/**
-	 * The port for the js5 server
-	 */
-	public static final int JS5_PORT_ID = 43595;
+    /**
+     * SQL will always be enabled if the server is hosted, otherwise we can toggle it on or off
+     */
+    public static boolean SQL_ENABLED = /*DEBUG ||*/ HOSTED;
 
-	/**
-	 * The maximum size of the avail that can be received
-	 */
-	public static final int RECEIVE_DATA_LIMIT = 7500;
+    /**
+     * If double votes are enabled
+     */
+    public static boolean DOUBLE_VOTES_ENABLED;
 
-	/**
-	 * The largest size of packet we can send
-	 */
-	public static final int PACKET_SIZE_LIMIT = 7500;
+    /**
+     * The times for double experieince
+     */
+    public static final long[] DOUBLE_EXPERIENCE_TIMES = new long[2];
 
-	/**
-	 * If gambling is enabled in the server (dicing/flower poker)
-	 */
-	public static final boolean GAMBLING_ENABLED = false;
+    /**
+     * The build of the client
+     */
+    public static final int CLIENT_BUILD = 667;
 
-	/**
-	 * Player settings
-	 */
-	public static final int START_PLAYER_HITPOINTS = 100;
+    /**
+     * The custom build of the client, shows what version we're on
+     */
+    public static final int CUSTOM_CLIENT_BUILD = 3;
 
-	public static final WorldTile START_PLAYER_LOCATION = new WorldTile(3086, 3502, 0);
+    /**
+     * The port the server listens on
+     */
+    public static final int GAME_PORT_ID = 43594;
 
-	public static final String START_CONTROLER = "StartTutorial";
+    /**
+     * The port for the js5 server
+     */
+    public static final int JS5_PORT_ID = 43595;
 
-	public static final WorldTile RESPAWN_PLAYER_LOCATION = new WorldTile(3102, 3492, 0);
+    /**
+     * The maximum size of the avail that can be received
+     */
+    public static final int RECEIVE_DATA_LIMIT = 7500;
 
-	/**
-	 * The chance multiplier for drops
-	 */
-	public static final double DROP_CHANCE_MULTIPLIER = 1.0;
+    /**
+     * The largest size of packet we can send
+     */
+    public static final int PACKET_SIZE_LIMIT = 7500;
 
-	/**
-	 * The experience rate multiplier for combat skills
-	 */
-	public static final int COMBAT_EXP_RATE = 400, IRONMAN_COMBAT_EXP_RATE = 10;
+    /**
+     * If gambling is enabled in the server (dicing/flower poker)
+     */
+    public static final boolean GAMBLING_ENABLED = false;
 
-	/**
-	 * The experience rate multiplier for non-combat skills
-	 */
-	public static final int SKIll_EXP_RATE = 10, IRONMAN_SKILL_EXP_RATE = 5;
+    /**
+     * Player settings
+     */
+    public static final int START_PLAYER_HITPOINTS = 100;
 
-	/**
-	 * The ironman exp rate for prayer
-	 */
-	public static final double IRONMAN_PRAYER_EXP_RATE = 5;
+    public static final WorldTile START_PLAYER_LOCATION = new WorldTile(3086, 3502, 0);
 
-	/**
-	 * The regular exp rate for prayer
-	 */
-	public static final double PRAYER_EXP_RATE = 10;
+    public static final String START_CONTROLER = "StartTutorial";
 
-	/**
-	 * The bonus multiplier for daily skills
-	 */
-	public static final double DAILY_EXP_BONUS = 1.15;
+    public static final WorldTile RESPAWN_PLAYER_LOCATION = new WorldTile(3102, 3492, 0);
 
-	/**
-	 * Music & Emote settings
-	 */
-	public static final int AIR_GUITAR_MUSICS_COUNT = 50;
+    /**
+     * The chance multiplier for drops
+     */
+    public static final double DROP_CHANCE_MULTIPLIER = 1.0;
 
-	/**
-	 * Memory settings
-	 */
-	public static final int PLAYERS_LIMIT = 2048;
-	
-	public static final int NPCS_LIMIT = Short.MAX_VALUE;
+    /**
+     * The experience rate multiplier for combat skills
+     */
+    public static final int COMBAT_EXP_RATE = 400, IRONMAN_COMBAT_EXP_RATE = 10;
 
-	public static final int LOCAL_NPCS_LIMIT = 250;
+    /**
+     * The experience rate multiplier for non-combat skills
+     */
+    public static final int SKIll_EXP_RATE = 10, IRONMAN_SKILL_EXP_RATE = 5;
 
-	public static final int MIN_FREE_MEM_ALLOWED = 30000000; // 30mb
+    /**
+     * The ironman exp rate for prayer
+     */
+    public static final double IRONMAN_PRAYER_EXP_RATE = 5;
 
-	/**
-	 * Game constants
-	 */
-	public static final int[] MAP_SIZES = { 104, 120, 136, 168 };
+    /**
+     * The regular exp rate for prayer
+     */
+    public static final double PRAYER_EXP_RATE = 10;
 
-	public static final int[] GRAB_SERVER_KEYS = { 1393, 78700, 44880, 39771, 363186, 44375, 0, 16140, 6028, 263849, 778481, 209109, 372444, 444388, 892700, 20013, 24356, 16747, 1244, 1, 13271, 1321, 119, 853169, 1748783, 3963, 3323 };
+    /**
+     * The bonus multiplier for daily skills
+     */
+    public static final double DAILY_EXP_BONUS = 1.15;
 
-	public static final int READ_BUFFER_SIZE = 20 * 1024; // 20kb
+    /**
+     * Music & Emote settings
+     */
+    public static final int AIR_GUITAR_MUSICS_COUNT = 50;
 
-	public static final int WRITE_BUFFER_SIZE = 20 * 1024; // 20kb
+    /**
+     * Memory settings
+     */
+    public static final int PLAYERS_LIMIT = 2048;
 
-	public static final String SQL_FILE_PATH = "./data/sql.txt";
+    public static final int NPCS_LIMIT = Short.MAX_VALUE;
 
-	/**
-	 * The path the cache will be loaded from
-	 */
-	public static final String CACHE_PATH = "./data/essentials/cache/";
+    public static final int LOCAL_NPCS_LIMIT = 250;
 
-	/**
-	 * The server is on hosted mode if the main user name contains 'Administrator'
-	 */
-	public static boolean HOSTED = System.getProperty("user.home").toLowerCase().contains("root") || LINUX_HOST;
+    public static final int MIN_FREE_MEM_ALLOWED = 30000000; // 30mb
 
-	/**
-	 * The path for files to be saved at
-	 */
-	public static final String FILES_PATH = HOSTED ? LINUX_HOST ? "/root/gamedata/" : "C:/gamedata/" : "data/";
+    /**
+     * Game constants
+     */
+    public static final int[] MAP_SIZES = {104, 120, 136, 168};
 
-	/**
-	 * We will always be on debug mode if the server isn't hosted
-	 */
-	public static boolean DEBUG = !HOSTED;
+    public static final int[] GRAB_SERVER_KEYS = {1393, 78700, 44880, 39771, 363186, 44375, 0, 16140, 6028, 263849, 778481, 209109, 372444, 444388, 892700, 20013, 24356, 16747, 1244, 1, 13271, 1321, 119, 853169, 1748783, 3963, 3323};
 
-	/**
-	 * SQL will always be enabled if the server is hosted, otherwise we can toggle it on or off
-	 */
-	public static boolean SQL_ENABLED = /*DEBUG ||*/ HOSTED;
+    public static final int READ_BUFFER_SIZE = 20 * 1024; // 20kb
 
-	/**
-	 * If double votes are enabled
-	 */
-	public static boolean DOUBLE_VOTES_ENABLED;
-	private GameConstants() {
+    public static final int WRITE_BUFFER_SIZE = 20 * 1024; // 20kb
 
-	}
+    public static final String SQL_FILE_PATH = "./data/sql.txt";
+
+    private GameConstants() {
+
+    }
 
 }
